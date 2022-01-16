9fc7684fa50cfb8457b92b1cb4b0b3e4a58e0b3b
Emilio Ziniades <emilioziniades@protonmail.com>
2022-01-16 17:53:36 +0200

    day 11 part 1 complete

--- day11/octopus.go
+++ day11/octopus.go
@@ -2,19 +2,103 @@ package day11
 
 import "fmt"
 
-func FlashCount(grid [][]int, steps int) int {
-	fmt.Println(grid, len(grid), cap(grid))
+var R = []int{-1, -1, -1, 0, 0, 1, 1, 1}
+var C = []int{-1, 0, 1, -1, 1, -1, 0, 1}
+
+func FlashCountOld(grid [][]int, steps int) int {
+	count := 0
+	fmt.Println("Before any steps:")
 	printGrid(grid)
 	h, w := len(grid), len(grid[0])
-	for i := 0; i < steps; i++ {
+	for i := 1; i <= steps; i++ {
+		for r := 0; r < h; r++ {
+			for c := 0; c < w; c++ {
+				grid[r][c]++
+				if grid[r][c] > 9 {
+					//flash
+					for n := 0; n < 8; n++ {
+						//flash neighbours
+						row, col := r+R[n], c+C[n]
+						if row >= 0 && row < h && col >= 0 && col < w {
+							grid[row][col]++
+						}
+					}
+				}
+			}
+		}
+		// set all values > 9 to 0
+		for r := 0; r < h; r++ {
+			for c := 0; c < w; c++ {
+				if grid[r][c] > 9 {
+					count++
+					grid[r][c] = 0
+
+				}
+			}
+		}
+		fmt.Printf("After step %d:\n", i)
+		printGrid(grid)
+	}
+	return count
+}
+
+func FlashCount(grid [][]int, steps int) int {
+	count := 0
+	fmt.Println("Before any steps:")
+	printGrid(grid)
+	h, w := len(grid), len(grid[0])
+	for i := 1; i <= steps; i++ {
+		// Step 1 : increase every octopus' energy level by 1
 		for r := 0; r < h; r++ {
 			for c := 0; c < w; c++ {
 				grid[r][c]++
 			}
 		}
-		printGrid(grid)
+
+		// Step 2 : Any octopus > 9 flashes. Neighbours increase by 1. Also flash if > 9
+		hasFlashed := make(map[point]bool)
+		flashed := queue{}
+		for r := 0; r < h; r++ {
+			for c := 0; c < w; c++ {
+				if grid[r][c] > 9 {
+					p := point{r, c}
+					flashed.Enqueue(p)
+					hasFlashed[p] = true
+				}
+			}
+		}
+
+		for len(flashed) > 0 {
+			curr := flashed.Dequeue()
+			//			fmt.Println("flashing", curr)
+			for n := 0; n < 8; n++ {
+				row, col := curr.R+R[n], curr.C+C[n]
+				if row >= 0 && row < h && col >= 0 && col < w {
+					grid[row][col]++
+					if grid[row][col] > 9 && !hasFlashed[point{row, col}] {
+						p := point{row, col}
+						flashed.Enqueue(p)
+						hasFlashed[p] = true
+					}
+				}
+			}
+		}
+		// Step 3: Any octopus that flashed is set to 0
+		for r := 0; r < h; r++ {
+			for c := 0; c < w; c++ {
+				if grid[r][c] > 9 {
+					count++
+					grid[r][c] = 0
+
+				}
+			}
+		}
+		if i%10 == 0 {
+			fmt.Printf("After step %d:\n", i)
+			printGrid(grid)
+		}
 	}
-	return 0
+	return count
 }
 
 func printGrid(grid [][]int) {
@@ -24,4 +108,5 @@ func printGrid(grid [][]int) {
 		}
 		fmt.Println("")
 	}
+	fmt.Println("")
 }
--- day11/octopus_test.go
+++ day11/octopus_test.go
@@ -5,15 +5,27 @@ import (
 	"testing"
 
 	"github.com/emilioziniades/adventofcode2021/day11"
+	"github.com/emilioziniades/adventofcode2021/fetch"
 	"github.com/emilioziniades/adventofcode2021/parse"
 )
+
+func init() {
+	err := fetch.Data("https://adventofcode.com/2021/day/11/input", "11-input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func TestFlashCount(t *testing.T) {
 	var tests = []struct {
 		file string
 		want int
+		days int
 	}{
-		{"11-example.txt", 1656},
+		{"11-simple.txt", 9, 2},
+		{"11-example.txt", 204, 10},
+		{"11-example.txt", 1656, 100},
+		{"11-input.txt", 0, 100},
 	}
 
 	for _, tt := range tests {
@@ -21,7 +33,7 @@ func TestFlashCount(t *testing.T) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		got := day11.FlashCount(in, 10)
+		got := day11.FlashCount(in, tt.days)
 		if got != tt.want {
 			t.Fatalf("got %d, wanted %d for %s", got, tt.want, tt.file)
 		}
