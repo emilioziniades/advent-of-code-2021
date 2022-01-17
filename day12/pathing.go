d8ba2d70ffe464b8eef67e09bdc4e5a766096a23
Emilio Ziniades <emilioziniades@protonmail.com>
2022-01-17 19:21:53 +0200

    day12 part 2 complete

--- day12/pathing.go
+++ day12/pathing.go
@@ -5,13 +5,84 @@ import (
 	"strings"
 )
 
-func CountPaths(in []string) int {
-	paths := pathsFromStringSlice(in)
-	fmt.Println(paths)
-	return 0
+func CountPaths(in []string, hasFunc func([]string, string) bool) int {
+	graph := graphFromStringSlice(in)
+	paths := make([][]string, 0)
+
+	var recPaths func([]string)
+	recPaths = func(s []string) {
+		curr := s[len(s)-1]
+		if curr == "end" {
+			paths = append(paths, s)
+			return
+		}
+
+		for _, e := range graph[curr] {
+			if e == "start" {
+				continue
+			}
+			if !hasFunc(s, e) || IsUpper(e) {
+				recPaths(append(s, e))
+			}
+		}
+	}
+
+	recPaths([]string{"start"})
+	return len(paths)
 }
 
-func pathsFromStringSlice(in []string) map[string][]string {
+func CountPathsOne(in []string) int {
+	return CountPaths(in, has)
+}
+
+func CountPathsTwo(in []string) int {
+	return CountPaths(in, hasAndLowerTwice)
+}
+
+func has(sl []string, s string) bool {
+	for _, e := range sl {
+		if e == s {
+			return true
+		}
+	}
+	return false
+}
+
+// hasAndLowerTwice should return true if:
+// - any lowercase cave appears twice AND s already included in sl
+// otherwise return false
+func hasAndLowerTwice(sl []string, s string) bool {
+
+	c := make(map[string]int)
+	var twice, has bool
+
+	for _, e := range sl {
+		if IsLower(e) {
+			c[e]++
+		}
+		if e == s {
+			has = true
+		}
+	}
+
+	// checks if any lowercase cave visited twice
+	for _, v := range c {
+		if v >= 2 {
+			twice = true
+		}
+	}
+
+	return twice && has
+}
+
+func IsUpper(s string) bool {
+	return s == strings.ToUpper(s)
+}
+func IsLower(s string) bool {
+	return s == strings.ToLower(s)
+}
+
+func graphFromStringSlice(in []string) map[string][]string {
 	gamma := make(map[string][]string)
 	for _, e := range in {
 		nodes := strings.Split(e, "-")
@@ -22,6 +93,6 @@ func pathsFromStringSlice(in []string) map[string][]string {
 	return gamma
 }
 
-func IsUpper(s string) bool {
-	return s == strings.ToUpper(s)
+func printPath(s []string) {
+	fmt.Println(strings.Join(s, ","))
 }
--- day12/pathing_test.go
+++ day12/pathing_test.go
@@ -5,15 +5,24 @@ import (
 	"testing"
 
 	"github.com/emilioziniades/adventofcode2021/day12"
+	"github.com/emilioziniades/adventofcode2021/fetch"
 	"github.com/emilioziniades/adventofcode2021/parse"
 )
 
+func init() {
+	fetch.Data("https://adventofcode.com/2021/day/12/input", "12-input.txt")
+}
+
 func TestCountPaths(t *testing.T) {
 	var tests = []struct {
-		file string
-		want int
+		file    string
+		want    int
+		wantTwo int
 	}{
-		{"12-simple.txt", 10},
+		{"12-simple.txt", 10, 36},
+		{"12-example.txt", 19, 103},
+		{"12-large.txt", 226, 3509},
+		{"12-input.txt", 3410, 98796},
 	}
 
 	for _, tt := range tests {
@@ -22,10 +31,23 @@ func TestCountPaths(t *testing.T) {
 			log.Fatal(err)
 		}
 
-		got := day12.CountPaths(in)
+		got := day12.CountPathsOne(in)
 		if got != tt.want {
 			t.Fatalf("got %d, wanted %d, for %s", got, tt.want, tt.file)
 		}
 		t.Logf("got %d, wanted %d, for %s", got, tt.want, tt.file)
+	}
+
+	for _, tt := range tests {
+		in, err := parse.FileToStringSlice(tt.file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		got := day12.CountPathsTwo(in)
+		if got != tt.wantTwo {
+			t.Fatalf("got %d, wanted %d, for %s", got, tt.wantTwo, tt.file)
+		}
+		t.Logf("got %d, wanted %d, for %s", got, tt.wantTwo, tt.file)
 	}
 }
