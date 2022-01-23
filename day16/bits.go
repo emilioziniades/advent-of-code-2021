2a65f0bbecebe5da54b3eacfc43f0b04ecc39870
Emilio Ziniades <emilioziniades@protonmail.com>
2022-01-23 10:04:04 +0200

    day 16 part 2 complete

--- day16/bits.go
+++ day16/bits.go
@@ -7,13 +7,16 @@ import (
 	"math/big"
 	"strconv"
 	"strings"
-
-	"github.com/emilioziniades/adventofcode2021/stack"
 )
 
 type item struct {
 	value int
 	op    bool
+}
+
+type tree struct {
+	it       item
+	children []*tree
 }
 
 var opMap = map[int]string{
@@ -22,20 +25,13 @@ var opMap = map[int]string{
 	2: "minimum",
 	3: "maximum",
 	4: "literal",
-	5: "less",
-	6: "greater",
+	5: "greater",
+	6: "less",
 	7: "equal",
-}
-
-type tree struct {
-	it item
-	children []*tree
 }
 
 var versionCount int
 var depth int
-
-var s stack.Stack[item]
 
 func Bits(file []string) (int, int) {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
@@ -49,23 +45,21 @@ func Bits(file []string) (int, int) {
 	if _, ok := dec.SetString(hex, 16); !ok {
 		log.Fatalf("Bits: converting %s", hex)
 	}
-	//pads with leading zeroes so that there are exactly 4 times as many bits as hex digits
+	//pads with leading zeroes so that num bits = 4 * hex digits
 	bin := fmt.Sprintf("%0*b", len(hex)*4, dec)
 	fmt.Println(bin, hex)
 
-	s = stack.New[item]()
-	r := strings.NewReader(bin)
 	versionCount = 0
-//	t := new(tree)
-	parsePacket(r, 100, -1)
-	//	fmt.Println("\nversion count: ", versionCount)
-	fmt.Println(s)
-	return versionCount, parseStack(s)
+	r := strings.NewReader(bin)
+	t := new(tree)
+	parsePacket(r, t, 100, -1)
+	t = t.children[0]
+	printTree(*t)
+	fmt.Println("")
+	return versionCount, reduceTree(t)
 }
 
-func parsePacket(r *strings.Reader, length, lengthType int) {
-	depth++
-	//	fmt.Printf("%*snew call to parsePacket with length %d, type ID %d and %d remaining bits\n", depth*4, "", length, lengthType, r.Len())
+func parsePacket(r *strings.Reader, t *tree, length, lengthType int) {
 	nRead := 0
 	for nRead < length {
 		version := readN(r, 3)
@@ -84,10 +78,9 @@ func parsePacket(r *strings.Reader, length, lengthType int) {
 					break
 				}
 			}
-			//			fmt.Printf("%*sliteral packet -> version: %d, type ID: %d, literal value: %d\n", depth*4, "", version, typeId, parseBits(value.String()))
 			v := parseBits(value.String())
-			s.Push(item{v, false})
-			fmt.Printf("%*sliteral -> %d\n", depth*4, "", v)
+			child := tree{item{v, false}, make([]*tree, 0)}
+			t.children = append(t.children, &child)
 		default:
 			lengthTypeId := readN(r, 1)
 			bitsRead += 1
@@ -101,11 +94,11 @@ func parsePacket(r *strings.Reader, length, lengthType int) {
 				bitsRead += 15
 			}
 
-			//			fmt.Printf("%*soperator packet -> version: %d, type ID: %d, length type ID: %d, length info: %d\n", depth*4, "", version, typeId, lengthTypeId, l)
-			fmt.Printf("%*soperator ->  %s\n", depth*4, "", opMap[typeId])
-			s.Push(item{typeId, true})
+			child := tree{item{typeId, true}, make([]*tree, 0)}
+			t.children = append(t.children, &child)
+
 			oldLen := r.Len()
-			parsePacket(r, l, lengthTypeId)
+			parsePacket(r, &child, l, lengthTypeId)
 			bitsRead += oldLen - r.Len()
 		}
 		switch lengthType {
@@ -120,82 +113,82 @@ func parsePacket(r *strings.Reader, length, lengthType int) {
 			nRead = length
 
 		}
-
-		//		fmt.Printf("%*sremaining bits: %d\n", depth*4, "", r.Len()
-		//		fmt.Printf("%*slength: %d, nRead: %d\n", depth*4, "", length, nRead)
 	}
-	depth--
 }
 
-func parseStack(s stack.Stack[item]) int {
-	valStack := stack.New[int]()
-	for len(s) > 0 {
-		x := s.Pop()
-		if x.op {
-			switch x.value {
-			case 0:
-				sum := 0
-				for _, e := range valStack {
-					sum += e
-				}
-				s.Push(item{sum, false})
-				valStack.Reset()
-			case 1:
-				product := 1
-				for _, e := range valStack {
-					product *= e
-				}
-				s.Push(item{product, false})
-				valStack.Reset()
-			case 2:
-				min := math.MaxInt
-				for _, e := range valStack {
-					if e < min {
-						min = e
-					}
-				}
-				s.Push(item{min, false})
-				valStack.Reset()
-			case 3:
-				max := 0
-				for _, e := range valStack {
-					if e > max {
-						max = e
-					}
-				}
-				s.Push(item{max, false})
-				valStack.Reset()
-			case 5:
-				first, second := valStack.Pop(), valStack.Pop()
-				greater := 0
-				if first > second {
-					greater = 1
-				}
-				s.Push(item{greater, false})
-				valStack.Reset()
-			case 6:
-				first, second := valStack.Pop(), valStack.Pop()
-				less := 0
-				if first < second {
-					less = 1
-				}
-				s.Push(item{less, false})
-				valStack.Reset()
-			case 7:
-				first, second := valStack.Pop(), valStack.Pop()
-				equal := 0
-				if first == second {
-					equal = 1
-				}
-				s.Push(item{equal, false})
-				valStack.Reset()
+func reduceTree(t *tree) int {
+	curr := t.it
+	if curr.op {
+		switch curr.value {
+		case 0:
+			sum := 0
+			for _, e := range t.children {
+				sum += reduceTree(e)
 			}
-		} else {
-			valStack.Push(x.value)
+			return sum
+		case 1:
+			product := 1
+			for _, e := range t.children {
+				product *= reduceTree(e)
+			}
+			return product
+		case 2:
+			min := math.MaxInt
+			for _, e := range t.children {
+				if r := reduceTree(e); r < min {
+					min = r
+				}
+			}
+			return min
+		case 3:
+			max := 0
+			for _, e := range t.children {
+				if r := reduceTree(e); r > max {
+					max = r
+				}
+			}
+			return max
+		case 5:
+			greater := 0
+			first, second := reduceTree(t.children[0]), reduceTree(t.children[1])
+			if first > second {
+				greater = 1
+			}
+			return greater
+		case 6:
+			less := 0
+			first, second := reduceTree(t.children[0]), reduceTree(t.children[1])
+			if first < second {
+				less = 1
+			}
+			return less
+		case 7:
+			equal := 0
+			first, second := reduceTree(t.children[0]), reduceTree(t.children[1])
+			if first == second {
+				equal = 1
+			}
+			return equal
+		default:
+			return 10000000
 		}
-//		fmt.Println(s, valStack)
+	} else {
+		return curr.value
 	}
-	return valStack[0]
+}
+
+func printTree(t tree) {
+	curr := t.it
+	if curr.op {
+		fmt.Printf("%*s%s\n", depth*4, "", opMap[curr.value])
+	} else {
+		fmt.Printf("%*s%d\n", depth*4, "", curr.value)
+	}
+	depth++
+	for _, e := range t.children {
+		printTree(*e)
+	}
+	depth--
 }
 
 //readN reads n runes from r and returns it as an int
